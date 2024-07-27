fa1e82a63fcf44fdc600c1064593ce1882bfe30f
Gabriel0018 <gabrielmadureira9@gmail.com>
2024-07-27 17:46:23 -0300

    Testes conclúidos - Pronto para Merge

--- __test__/youtubeServices.test.js
+++ __test__/youtubeServices.test.js
@@ -3,7 +3,7 @@ const videoData = require('../__test__/json/dataMock.json');
 const axios = require('axios');
 require('dotenv').config({path: '.env'});
 
-describe("Testes de verificação de chamada da API", () => {
+describe("Testes de verificação de chamada da API do YouTube", () => {
     test('API deve retornar 200 ao ser chamada corretamente', async () => {
         const API_KEY = process.env.YOUTUBE_API_KEY;
 
@@ -32,18 +32,83 @@ describe("Testes de verificação de chamada da API", () => {
     })
 })
 
-describe('Testes de título: deve retornar corretamente o título do vídeo', () => {
-    videoData.videos.forEach(video => {
-        test(`vídeo com ID ${video.videoId}`, async () => {
-            const videoId = video.videoId;
+describe('Testes de exceções', () => {
+    test('deve retornar Vídeo não encontrado ao informar ID de vídeo inválida', async () => {
+        const videoId = "id-invalida";
 
-            const title = await youtubeServices.getTitle(videoId);
-            
-            expect(title).toEqual(video.title);
+        const title = await youtubeServices.getTitle(videoId);
+        
+        expect(title).toEqual("Vídeo não encontrado");
+    })
+})
+
+describe('Testes de título', () => {
+    describe('deve retornar corretamente o título do vídeo', () => {
+        videoData.videos.forEach(video => {
+            test(`link do vídeo: ${video.url}`, async () => {
+                const videoId = video.videoId;
+    
+                const title = await youtubeServices.getTitle(videoId);
+                
+                expect(title).toEqual(video.title);
+            });
         });
-    });
+    })
 });
 
 describe('Testes de descrição', () => {
+    describe('deve retornar corretamente que a 1ᵃ oração da descrição está no vídeo', () => {
+        videoData.videos.forEach(video => {
+            test(`link do vídeo: ${video.url}`, async () => {
+                const videoId = video.videoId;
     
+                const description = await youtubeServices.getDescription(videoId);
+                
+                expect(description).toContain(video.firstSentenceDescription);
+            });
+        });
+    })
+});
+
+describe('Testes de views', () => {
+    describe('deve retornar corretamente que a quantidade de views estão no intervalo de 1000 até 2000000', () => {
+        videoData.videos.forEach(video => {
+            test(`link do vídeo: ${video.url}`, async () => {
+                const videoId = video.videoId;
+    
+                const views = await youtubeServices.getViews(videoId);
+                
+                expect(Number(views)).toBeGreaterThanOrEqual(1000);
+                expect(Number(views)).toBeLessThanOrEqual(200000)
+            });
+        });
+    })
+});
+
+describe('Testes de likes', () => {
+    describe('deve retornar corretamente que a quantidade de likes estão no intervalo de 100 até 200000', () => {
+        videoData.videos.forEach(video => {
+            test('link do vídeo ${video.url}', async () => {
+                const videoId = video.videoId;
+    
+                const likes = await youtubeServices.getLikes(videoId);
+                expect(Number(likes)).toBeGreaterThanOrEqual(100);
+                expect(Number(likes)).toBeLessThanOrEqual(200000);
+            });
+        });
+    })
+});
+
+describe('Testes de data de publicação do vídeo', () => {
+    describe('deve retornar corretamente ano, mês e dia da publicação do vídeo', () => {
+        videoData.videos.forEach(video => {
+            test(`link do vídeo: ${video.url}`, async () => {
+                const videoId = video.videoId;
+    
+                const creationDate = await youtubeServices.getCreationDate(videoId);
+                
+                expect(creationDate).toEqual(video.creationDate);
+            });
+        });
+    })
 });
--- src/youtubeServices.js
+++ src/youtubeServices.js
@@ -48,15 +48,85 @@ const youtubeServices = {
         } catch (error) {
             return 'Erro ao buscar título do vídeo';
         }
+    },
+
+    getViews: async function(videoId){
+        try {
+            const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+                params: {
+                    part: 'statistics',
+                    id: videoId,
+                    key: process.env.YOUTUBE_API_KEY
+                }
+            });
+    
+            const video = response.data.items[0];
+    
+            if (video) {
+                return video.statistics.viewCount;
+
+            } else {
+                return 'Vídeo não encontrado';
+            }
+    
+        } catch (error) {
+            return 'Erro ao buscar título do vídeo';
+        }
+    },
+
+    getLikes: async function(videoId){
+        try {
+            const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+                params: {
+                    part: 'statistics',
+                    id: videoId,
+                    key: process.env.YOUTUBE_API_KEY
+                }
+            });
+    
+            const video = response.data.items[0];
+    
+            if (video) {
+                return video.statistics.likeCount;
+
+            } else {
+                return 'Vídeo não encontrado';
+            }
+    
+        } catch (error) {
+            return 'Erro ao buscar título do vídeo';
+        }
+    },
+
+    getCreationDate: async function(videoId){
+        try {
+            const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+                params: {
+                    part: 'snippet',
+                    id: videoId,
+                    key: process.env.YOUTUBE_API_KEY
+                }
+            });
+    
+            const video = response.data.items[0];
+    
+            if (video) {
+                const publishedAt = video.snippet.publishedAt;
+                const date = new Date(publishedAt);
+                const year = date.getFullYear();
+                const month = String(date.getMonth() + 1).padStart(2, '0'); // Adiciona zero à esquerda se necessário
+                const day = String(date.getDate()).padStart(2, '0');       // Adiciona zero à esquerda se necessário
+
+                return `${year}-${month}-${day}`;
+
+            } else {
+                return 'Vídeo não encontrado';
+            }
+    
+        } catch (error) {
+            return 'Erro ao buscar data de criação do vídeo';
+        }
     }
 }
-/*
-youtubeServices.getTitle('rN3YnSg0WjM').then(getTitle => {
-    console.log('Título do vídeo:', getTitle)
-})
 
-youtubeServices.getDescription('rN3YnSg0WjM').then(getDescription => {
-    console.log('Descrição do vídeo:', getDescription);
-});
-*/
 module.exports = youtubeServices;
